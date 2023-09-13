b0d32ede8e78a9dec3162a2b77bc1cbf053d4a2c
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-09-13 10:07:38 -0300

    :sparkles: feat: categories module

--- api/src/shared/database/database.module.ts
+++ api/src/shared/database/database.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common';
 
 import { PrismaService } from './prisma.service';
+
 import { UsersRepository } from './repositories/users.repository';
+import { CategoriesRepository } from './repositories/categories.repository';
 
 @Global()
 @Module({
-  providers: [PrismaService, UsersRepository],
-  exports: [UsersRepository],
+  providers: [PrismaService, UsersRepository, CategoriesRepository],
+  exports: [UsersRepository, CategoriesRepository],
 })
 export class DatabaseModule {}
