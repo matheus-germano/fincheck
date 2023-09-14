7f5c029d82efcaa2fc541732b0fa0caa3b40c06b
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-09-14 12:16:47 -0300

    :lipstick: feat: sign in and sign up pages

--- frontend/src/view/pages/SignUp/index.tsx
+++ frontend/src/view/pages/SignUp/index.tsx
@@ -1,5 +1,50 @@
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+
 export function SignUp() {
   return (
-    <h1>Sign Up Page</h1>
+    <>
+      <header className="flex flex-col items-center gap-4 text-center">
+        <h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">
+          Crie sua conta
+        </h1>
+        <p className="space-x-2">
+          <span className="text-gray-700 tracking-[-0.5px]">
+            Já possui uma conta?
+          </span>
+          <Link
+            to="/sign-in"
+            className="text-teal-900 font-medium tracking-[-0.5px]"
+          >
+            Faça login
+          </Link>
+        </p>
+      </header>
+
+      <form className="mt-[60px] flex flex-col gap-4">
+        <Input
+          name="name"
+          type="text"
+          placeholder="Nome"
+        />
+        <Input
+          name="email"
+          type="email"
+          placeholder="E-mail"
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+        <Button
+          type="submit"
+          className="mt-2"
+        >
+          Criar conta
+        </Button>
+      </form>
+    </>
   )
 }
