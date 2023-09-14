41b0a54dfa3b50ae3fccfece30cde341985160ac
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-09-14 12:11:50 -0300

    :sparkles: feat: button component

---
+++ frontend/src/view/components/Button.tsx
@@ -0,0 +1,14 @@
+import { ComponentProps, ReactNode } from "react";
+
+interface ButtonProps extends ComponentProps<"button"> {
+  children: ReactNode
+}
+
+export function Button({ ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className="bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all"
+    />
+  )
+}
