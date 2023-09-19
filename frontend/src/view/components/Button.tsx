a94d6b6309e6b8a153e51e4a35154a904d9cceb3
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-09-18 23:14:33 -0300

    :sparkles: feat: loading state on butto

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,18 +1,23 @@
-import { ComponentProps, ReactNode } from 'react';
+import { ComponentProps } from 'react';
 import { cn } from '../../app/utils/cn';
+import { Spinner } from './Spinner';
 
 interface ButtonProps extends ComponentProps<'button'> {
-  children: ReactNode
+  isLoading?: boolean
 }
 
-export function Button({ className, ...props }: ButtonProps) {
+export function Button({ className, isLoading, disabled, children, ...props }: ButtonProps) {
   return (
     <button
       {...props}
+      disabled={disabled || isLoading}
       className={cn(
-        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all',
+        'bg-teal-900 hover:bg-teal-800 disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all flex items-center justify-center',
         className
       )}
-    />
+    >
+      {!isLoading && children}
+      {isLoading && <Spinner className="w-6 h-6" />}
+    </button>
   );
 }
